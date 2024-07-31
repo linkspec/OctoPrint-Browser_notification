ff893f70bb74e3dc03d8345030fcccafa0a90166
linkspec <8082399+linkspec@users.noreply.github.com>
2024-07-31 20:11:19 +0100

    Adding templates

--- octoprint_browser_notification/__init__.py
+++ octoprint_browser_notification/__init__.py
@@ -1,78 +1,23 @@
-# coding=utf-8
-from __future__ import absolute_import
-
-### (Don't forget to remove me)
-# This is a basic skeleton for your plugin's __init__.py. You probably want to adjust the class name of your plugin
-# as well as the plugin mixins it's subclassing from. This is really just a basic skeleton to get you started,
-# defining your plugin as a template plugin, settings and asset plugin. Feel free to add or remove mixins
-# as necessary.
-#
-# Take a look at the documentation on what other plugin mixins are available.
-
 import octoprint.plugin
 
-class Browser_notificationPlugin(octoprint.plugin.SettingsPlugin,
-    octoprint.plugin.AssetPlugin,
-    octoprint.plugin.TemplatePlugin
-):
+class BrowserNotificationsPlugin(octoprint.plugin.StartupPlugin,
+                                 octoprint.plugin.TemplatePlugin,
+                                 octoprint.plugin.SettingsPlugin,
+                                 octoprint.plugin.AssetPlugin):
+    def on_after_startup(self):
+        self._logger.info("Browser notifications - Hello world! (more: %s)" % self._settings.get(["url"]))
 
-    ##~~ SettingsPlugin mixin
 
     def get_settings_defaults(self):
-        return {
-            # put your plugin's default settings here
-        }
+        return dict(url="https://en.wikipedia.org/wiki/Hello_world")
+    
+    def get_template_vars(self):
+        return dict(url=self._settings.get(["url"]))
 
-    ##~~ AssetPlugin mixin
+__plugin_pythoncompat__ = ">=3.7,<4"
+__plugin_implementation__ = BrowserNotificationsPlugin()
 
-    def get_assets(self):
-        # Define your plugin's asset files to automatically include in the
-        # core UI here.
-        return {
-            "js": ["js/browser_notification.js"],
-            "css": ["css/browser_notification.css"],
-            "less": ["less/browser_notification.less"]
-        }
-
-    ##~~ Softwareupdate hook
-
-    def get_update_information(self):
-        # Define the configuration for your plugin to use with the Software Update
-        # Plugin here. See https://docs.octoprint.org/en/master/bundledplugins/softwareupdate.html
-        # for details.
-        return {
-            "browser_notification": {
-                "displayName": "Browser_notification Plugin",
-                "displayVersion": self._plugin_version,
-
-                # version check: github repository
-                "type": "github_release",
-                "user": "linkspec",
-                "repo": "OctoPrint-Browser_notification",
-                "current": self._plugin_version,
-
-                # update method: pip
-                "pip": "https://github.com/linkspec/OctoPrint-Browser_notification/archive/{target_version}.zip",
-            }
-        }
-
-
-# If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
-# ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
-# can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
-__plugin_name__ = "Browser_notification Plugin"
-
-
-# Set the Python version your plugin is compatible with below. Recommended is Python 3 only for all new plugins.
-# OctoPrint 1.4.0 - 1.7.x run under both Python 3 and the end-of-life Python 2.
-# OctoPrint 1.8.0 onwards only supports Python 3.
-__plugin_pythoncompat__ = ">=3,<4"  # Only Python 3
-
-def __plugin_load__():
-    global __plugin_implementation__
-    __plugin_implementation__ = Browser_notificationPlugin()
-
-    global __plugin_hooks__
-    __plugin_hooks__ = {
-        "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
-    }
+def get_assets(self):
+     return dict(
+         js=["js/browser_notification.js"]
+     )
